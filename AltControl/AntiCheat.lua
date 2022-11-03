2ea195a9f11e288f912eabd8237cb2e76e50de68
INoobie <114300660+INoobie@users.noreply.github.com>
2022-11-03 17:39:23 +0000

    Create AntiCheat.lua

---
+++ AltControl/AntiCheat.lua
@@ -0,0 +1,22 @@
+local hooking;
+hooking = hookmetamethod(game, '__index', function(a,b)
+    if a == 'Humanoid' and b == 'WalkSpeed' then
+        return 16
+    elseif a == 'Humanoid' and b == 'JumpPower' then
+        return 50
+    elseif a == 'Humanoid' and b == 'JumpHeight' then
+        return 7.1
+    end
+    return hooking(a,b)
+end)
+
+ local anticheat
+ local badremotes = {"CHECKER_1","CHECKER_2","TeleportDetect","OneMoreTime","BreathingHAMON","VirusCough"}
+ anticheat = hookmetamethod(game, '__namecall', function(...)
+    local args = {...}
+    local method = getnamecallmethod()
+    if(method == 'FireServer' and args[1].Name:lower() == 'mainevent' and table.find(badremotes, args[2])) then
+        return
+    end
+    return anticheat(table.unpack(args))
+ end)
